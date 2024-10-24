80623f3e3a61c90f5bfc00bd5d8b19cc1bf6aaf8
yoon <llsee01@naver.com>
2024-10-24 14:41:50 +0900

    test 데이터 전송하는 API 설정

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,19 @@ import { createStore } from "vuex"; // Vuex 4 방식으로 import
 
 export default createStore({
   state: {
+    // 상태 정의
     message: "Hello, Vuex!",
   },
   mutations: {
+    // 상태 변경 로직 정의
     setMessage(state, newMessage) {
       state.message = newMessage;
     },
   },
-  actions: {},
-  modules: {},
+  actions: {
+    // 비동기 작업 정의
+  },
+  modules: {
+    // 모듈로 스토어 분리 하기
+  },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <h1>멘토스 메인페이지용</h1>
-    <router-link to="/about">Go to About Page</router-link>
+    <router-link to="/about">Go to About Page</router-link><br />
+    <button style="margin-top: 10px" @click="sendTestMessage">
+      통신테스트
+    </button>
+    <b style="margin-left: 10px">클릭하면 "goodJob" 이라는 텍스트 전송</b>
   </div>
 </template>
 
 <script>
+import { sendTestData } from "../api/user";
+
 export default {
   name: "MainPage",
+  methods: {
+    sendTestMessage() {
+      // API 호출
+      sendTestData({ message: "goodJob" })
+        .then((response) => {
+          console.log("Response : ", response.data);
+        })
+        .catch((error) => {
+          console.error("Error: ", error);
+        });
+    },
+  },
 };
 </script>
