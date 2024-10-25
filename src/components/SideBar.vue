e99a0106d883722247c22c35fd3c759c627457ca
yoon <llsee01@naver.com>
2024-10-25 10:24:10 +0900

    sideBar 추가

---
+++ src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li><router-link to="/">메뉴1</router-link></li>
+      <li><router-link to="/about">메뉴2</router-link></li>
+      <li><router-link to="/test">메뉴3</router-link></li>
+      <li><router-link to="/test">메뉴4</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+};
+</script>
+
+<style scoped>
+.sidebar {
+  width: 200px;
+  height: 100vh;
+  background-color: #f4f4f4;
+  padding: 20px;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar li {
+  margin-bottom: 30px;
+}
+
+.sidebar a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    <h1>멘토스 메인페이지용</h1>
-    <router-link to="/about">Go to About Page</router-link><br />
-    <router-link to="/test">테스트 페이지 gaboja~</router-link><br />
+  <div class="main-container">
+    <Sidebar />
+    <div class="content">
+      <h1>멘토스 메인페이지용</h1>
+      <router-link to="/about">Go to About Page</router-link><br />
+      <router-link to="/test">테스트 페이지 gaboja~</router-link><br />
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from "@/components/SideBar.vue";
+
 export default {
   name: "MainPage",
   data() {},
   methods: {},
+  components: {
+    Sidebar,
+  },
 };
 </script>
+
+<style scoped>
+.main-container {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+</style>
