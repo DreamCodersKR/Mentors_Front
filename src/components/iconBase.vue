0cca181cc1047cd6eb45fb75bd289a9171a2f7cf
byeol <kimbyul5633@gmail.com>
2024-11-01 17:37:49 +0900

    byul : sidebarIcon 추가

---
+++ src/components/iconBase.vue
@@ -0,0 +1,28 @@
+<template>
+<svg
+    :width = "width" :height="height" :viewBox="viewBox" fill="none" 
+    xmlns="http://www.w3.org/2000/svg">
+    <path :d="d" :fill="iconColor"></path>
+</svg>
+
+</template>
+
+<script>
+export default {
+    props : {
+        width : {type : [Number, String], default: 18},
+        height : {type : [Number, String], default: 18},
+        viewBox : {type : String, defalut : "0 0 23 22"},
+        iconColor : {type : String, defalut : "#7980AB"},
+        iconName : {type : String, default : ""},
+        d : {type : String},
+        
+    }
+}
+</script>
+
+<style>
+svg{
+    margin : 10px;
+}
+</style>
